1f00e2e814433ff63107424e631ef8f327f8f371
Thomas <149729621+tactfulbear@users.noreply.github.com>
2026-09-22 16:40:26 +0200

    Abschnitte für die Uni-Fächer

--- quiz-app/src/seite/App.vue
+++ quiz-app/src/seite/App.vue
@@ -4,6 +4,11 @@
 
   <main id="main">
     <Nachhilfeangebot />
+    <MatheUni />
+    <InfoUni />
+    <PhysikUni />
+    <Ingenieurwesen />
+    <Chemie />
   </main>
 </template>
 
@@ -11,4 +16,9 @@
 import VueHeader from "./VueHeader.vue";
 import VueHero from "./VueHero.vue";
 import Nachhilfeangebot from "./abschnitte/Nachhilfeangebot.vue";
+import MatheUni from "./abschnitte/MatheUni.vue";
+import InfoUni from "./abschnitte/InfoUni.vue";
+import PhysikUni from "./abschnitte/PhysikUni.vue";
+import Ingenieurwesen from "./abschnitte/Ingenieurwesen.vue";
+import Chemie from "./abschnitte/Chemie.vue";
 </script>
---
+++ quiz-app/src/seite/abschnitte/Ingenieurwesen.vue
@@ -0,0 +1,29 @@
+<template>
+  <VueSection id="ingenieurwesen" titel="Ingenieurwesen">
+    <div class="row">
+      <div class="col-lg-6 order-1 order-lg-2" data-aos="fade-left" data-aos-delay="100">
+        <img src="assets/img/ingenieurwesen.png" class="img-fluid" alt="">
+      </div>
+      <div class="col-lg-6 pt-4 pt-lg-0 order-2 order-lg-1 content">
+        <p>Als ich vor Jahren in USA war, habe ich gelernt, dass
+          "German Engineering" dort bewundert wird.</p>
+        <p>
+          In der Nachhilfe können wir machen: Technische Mechanik (<a href="Pdfs/TechnischeMechanik/Nachhilfe Balbine 15.12.2021.pdf">1</a>,
+          <a href="Pdfs/TechnischeMechanik/Nachhilfe Hicham 31.12.2021.pdf">2</a>),
+          Elektrotechnik (<a href="Pdfs/Elektrotechnik/Nachhilfe Raphael 26.10.2022.pdf">1</a>, <a href="Pdfs/Elektrotechnik/Nachhilfe Shir 4.7.2021.pdf">2</a>, <a href="Pdfs/Elektrotechnik/Nachhilfe Shir 11.7.2021.pdf">3</a>),
+          Schaltungstechnik (<a href="Pdfs/Schaltungstechnik/Nachhilfe Christopher 20.4.2024.pdf">1</a>, <a href="Pdfs/Schaltungstechnik/Nachhilfe Christopher 28.4.2024.pdf">2</a>),
+          Regelungstechnik, Messtechnik und Strömungslehre (<a href="Pdfs/Strömungslehre/SL1.pdf">1</a>,
+          <a href="Pdfs/Strömungslehre/Nachhilfe Gwendolin 30.10.2021.pdf">2</a>,
+          <a href="Pdfs/Strömungslehre/Nachhilfe Wei Liu 27.3.2022.pdf">3</a>), Signalverarbeitung (<a href="Pdfs/Signalverarbeitung/Nachhilfe Son 11.8.2024 Teil 2.pdf">1</a>,<a href="Pdfs/Signalverarbeitung/Nachhilfe Nguyen 11.8.2024.pdf">2</a>),
+          Maschinenelemente, Baustatik, Technisches Zeichnen
+          <!--   Regelungstechnik -->
+
+        </p>
+      </div>
+    </div>
+  </VueSection>
+</template>
+
+<script setup lang="ts">
+import VueSection from "../VueSection.vue";
+</script>
